3f2bf88e75b0325252d88f9351acbed58998c147
yoonhyeyoon <hyeyun3306@gmail.com>
2022-01-17 11:38:12 +0900

    ➕ [feat] Add Signup, Login  loutes

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,11 +1,26 @@
-import Header from "./main/components/Header/Header";
-import Footer from "./main/components/Footer/Footer";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Header from "./components/main/Header/Header";
+import Footer from "./components/main/Footer/Footer";
+import Login from "./routes/Login";
+import Signup from "./routes/Signup";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
+      <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+      </Router>
+
       <Footer />
     </div>
   );
