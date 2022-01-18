ff5e0f174f628fc49b2be3cc7382cdbc838a5123
Minjeong Kim <lona573@gmail.com>
2022-01-18 13:52:51 +0900

    ✨ [feat] Update Login component
    <contents>
    1. 로그인 할때 아이디를 이메일 형식으로 입력했는지 검사 후 alert

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/main/Header/Header";
 import Footer from "./components/main/Footer/Footer";
 import Login from "./routes/account/Login";
 import Signup from "./routes/account/Signup";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
-      <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-      </Router>
+
+      <div className="main">
+        <Router>
+          <Routes>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+          </Routes>
+        </Router>
+      </div>
 
       <Footer />
     </div>
--- frontend-react/src/routes/account/Login.jsx
+++ frontend-react/src/routes/account/Login.jsx
@@ -1,4 +1,10 @@
-function Login() {
-  return <div>로그인</div>;
-}
+import LoginForm from "../../components/main/LoginForm/LoginForm";
+
+const Login = () => {
+  return <LoginForm />;
+};
+
+// function Login() {
+//   return <div>로그인</div>;
+// }
 export default Login;
