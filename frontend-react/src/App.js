a0bff7735267ca3a9919dceb0a7f95b06ef02038
yoonhyeyoon <hyeyun3306@gmail.com>
2022-01-19 10:54:39 +0900

    :hammer: [feat] Build logout, main

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,17 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import Header from "./components/main/Header/Header";
 import Footer from "./components/main/Footer/Footer";
 import Login from "./routes/account/Login";
 import Signup from "./routes/account/Signup";
+import Main from "./routes/main/Main";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("JWT");
+
+    if (token) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  });
+
   return (
     <div className="App">
       <Header />
 
       <Router>
         <Routes>
+          <Route path="/main" element={<Main isLogin={isLogin} />}></Route> :
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
---
+++ frontend-react/src/routes/main/Main.jsx
@@ -0,0 +1,25 @@
+function Main(isLogin) {
+  const onLogout = () => {
+      localStorage.removeItem('JWT');
+      setIsLogin(false);
+      // App 으로 이동 (새로고침)
+      document.location.href = '/';
+    }
+
+  return (
+    <div>
+      {isLogin ? (
+        <button
+          type="button"
+          onClick={onLogout}
+        >로그아웃</button>
+      ) : (
+        <div>
+          <button>회원가입</button>
+          <button>로그인</button>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Main;
