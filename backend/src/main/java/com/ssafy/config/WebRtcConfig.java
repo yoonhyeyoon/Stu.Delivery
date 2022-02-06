1bf287a70c9937790281e28f32c77b84811904e2
JosephNaa <js.pekah@gmail.com>
2022-02-07 02:02:27 +0900

    🔨 [feat] Build kurento --- 나요셉

---
+++ backend/src/main/java/com/ssafy/config/WebRtcConfig.java
@@ -0,0 +1,50 @@
+package com.ssafy.config;
+
+import com.ssafy.api.kurento.CallHandler;
+import com.ssafy.api.kurento.RoomManager;
+import com.ssafy.api.kurento.UserRegistry;
+import org.kurento.client.KurentoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+@Configuration
+@EnableWebSocket
+public class WebRtcConfig implements WebSocketConfigurer {
+
+    @Bean
+    public UserRegistry registry() {
+        return new UserRegistry();
+    }
+
+    @Bean
+    public RoomManager roomManager() {
+        return new RoomManager();
+    }
+
+    @Bean
+    public CallHandler groupCallHandler() {
+        return new CallHandler();
+    }
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(32768);
+        return container;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(groupCallHandler(), "/groupcall")
+            .setAllowedOrigins("*");
+    }
+}
