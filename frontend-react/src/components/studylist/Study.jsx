65d1d1fe2bf358860bc068bf1cb48c3e986ec610
yoonhyeyoon <hyeyun3306@gmail.com>
2022-02-14 19:27:56 +0900

    ✨ [feat] Update url, router

--- frontend-react/src/components/studylist/Study.jsx
+++ frontend-react/src/components/studylist/Study.jsx
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import StudyCheckPwd from "./StudyCheckPwd";
+import { useSelector, useDispatch } from "react-redux";
+import { is_member_check } from "../../utils/api";
+import axios from "axios";
+import { loadStudy } from "../../redux/study";
 
 const Study = ({ study }) => {
   const [show, setShow] = useState(false);
-  console.log(study);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchStudyInfo = async () => {
+      axios({
+        method: "get",
+        url: `https://i6d201.p.ssafy.io/api/v1/study/${study.id}`,
+      })
+        .then((res) => {
+          // console.log(res);
+          dispatch(loadStudy(res.data));
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchStudyInfo();
+  }, []);
+  const user = useSelector((state) => state.user.user);
+  const studyInfo = useSelector((state) => state.study.study);
+  const isMember = is_member_check(studyInfo, user);
+  // console.log(studyInfo, user, isMember);
+
   const handleClose = () => setShow(false);
   const handleShow = () => {
-    if (study) {
-      if (study.is_private) {
-        setShow(true);
+    if (study && isMember) {
+      console.log(isMember);
+      if (isMember) {
+        window.location.href = `/study/${study.id}`;
       } else {
-        window.location.href = `/study/${study.id}/info`;
+        if (study.is_private) {
+          setShow(true);
+        } else {
+          window.location.href = `/study/${study.id}/info`;
+        }
       }
     }
   };
