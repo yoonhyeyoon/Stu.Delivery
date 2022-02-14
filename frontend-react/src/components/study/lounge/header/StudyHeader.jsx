c363c6dcc4679d233aeaf28e91ef008a2080d345
yoonhyeyoon <hyeyun3306@gmail.com>
2022-02-14 13:21:16 +0900

    ✨ [feat] Update webrtc StateToProps

--- frontend-react/src/components/study/lounge/header/StudyHeader.jsx
+++ frontend-react/src/components/study/lounge/header/StudyHeader.jsx
@@ -8,44 +8,54 @@ import styles from "./StudyHeader.module.css";
 import StudyNameUrl from "./StudyNameUrl";
 import StudyBtn from "./StudyBtn";
 import Button from "react-bootstrap/Button";
+import VideoRoomComponent from "../../../webrtc/VideoRoomComponent";
 // import Entrance from "./Entrance";
 
 function StudyHeader() {
   const study = useSelector((state) => state.study.study);
-  const handlePrivate = () => {
-    axios({
-      method: "post",
-      url: `https://i6d201.p.ssafy.io:8443/openvidu/api/sessions/${study.private_room_id}/connection`,
-      headers: {
-        Authorization: `Basic btoa("OPENVIDUAPP:" + "STUDELIVERY")`,
-      },
-    })
-      .then((res) => {
-        console.log(res.data);
-        // window.location.reload();
-      })
-      .catch((err) => {
-        console.log(err);
-        // alert(err.response.data.errorMessage);
-      });
-  };
-  const handleMeeting = () => {
-    axios({
-      method: "post",
-      url: `https://i6d201.p.ssafy.io:8443/openvidu/api/sessions/${study.meeting_room_id}/connection`,
-      headers: {
-        Authorization: `Basic btoa("OPENVIDUAPP:" + "STUDELIVERY")`,
-      },
-    })
-      .then((res) => {
-        console.log(res.data);
-        // window.location.reload();
-      })
-      .catch((err) => {
-        console.log(err);
-        // alert(err.response.data.errorMessage);
-      });
-  };
+  const user = useSelector((state) => state.user.user);
+
+  // const handlePrivate = () => {
+  //   axios({
+  //     method: "post",
+  //     url: `https://i6d201.p.ssafy.io:8443/openvidu/api/sessions/${study.private_room_id}/connection`,
+  //     headers: {
+  //       Authorization: `Basic btoa("OPENVIDUAPP:" + "STUDELIVERY")`,
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       // window.location.reload();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       // alert(err.response.data.errorMessage);
+  //     });
+  // };
+  // const handleMeeting = () => {
+  //   axios({
+  //     method: "post",
+  //     url: `https://i6d201.p.ssafy.io:8443/openvidu/api/sessions/${study.meeting_room_id}/connection`,
+  //     headers: {
+  //       Authorization: `Basic btoa("OPENVIDUAPP:" + "STUDELIVERY")`,
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       // window.location.reload();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       // alert(err.response.data.errorMessage);
+  //     });
+  // };
+
+  // const handleGo = () => {
+  //   <VideoRoomComponent
+  //     openviduSecret={study.meeting_room_id}
+  //     user={user.name}
+  //   />;
+  // };
   return (
     <div className={styles.frame}>
       {study ? (
@@ -57,12 +67,20 @@ function StudyHeader() {
                 <Button className={styles.btn}>
                   <Link to={`/study/${study.id}/info`}>스터디 정보</Link>
                 </Button>
-                <Button className={styles.btn} onClick={handlePrivate}>
-                  <Link to={`/study/${study.id}`}>개인 열람실</Link>
-                </Button>
-                <Button className={styles.btn} onClick={handleMeeting}>
-                  <Link to={`/study/${study.id}`}>회의실</Link>
-                </Button>
+                {/* <Button className={styles.btn} onClick={handleGo}>
+                  개인 열람실
+                </Button> */}
+                <Link
+                  to={{
+                    pathname: `/study/${study.id}/webrtc`,
+                    state: {
+                      openviduSecret: study.meeting_room_id,
+                      user: user.name,
+                    },
+                  }}
+                >
+                  <Button className={styles.btn}>회의실</Button>
+                </Link>
               </>
             ) : (
               <StudyBtn />
