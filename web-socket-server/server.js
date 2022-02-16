e47e75cefac00df319c69317ea970096bda41708
kgw012 <kgw012@gmail.com>
2022-02-16 18:48:27 +0900

    🐛 [fix] Fix wss connet --- 김길웅

--- web-socket-server/server.js
+++ web-socket-server/server.js
@@ -1,24 +1,31 @@
-var http = require('http')
-var express = require('express')
-var WebSocket = require('ws')
-var ShareDB = require('sharedb')
-var WebSocketJSONStream = require('@teamwork/websocket-json-stream')
+var https = require("https");
+var express = require("express");
+var WebSocket = require("ws");
+var ShareDB = require("sharedb");
+var WebSocketJSONStream = require("@teamwork/websocket-json-stream");
+var fs = require("fs");
 
-console.log("load wss")
+console.log("load wss");
 
-var app = express()
-var server = http.createServer(app)
-var webSocketServer = new WebSocket.Server({server: server})
+var app = express();
 
-console.log("connect wss")
+const options = {
+  ca: fs.readFileSync("/etc/letsencrypt/live/i6d201.p.ssafy.io/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/i6d201.p.ssafy.io/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/i6d201.p.ssafy.io/cert.pem"),
+};
+var server = https.createServer(options, app);
+var webSocketServer = new WebSocket.Server({ server: server });
 
-ShareDB.types.register(require('rich-text').type);
-var backend = new ShareDB()
-webSocketServer.on('connection', (webSocket) => {
-  var stream = new WebSocketJSONStream(webSocket)
-  backend.listen(stream)
-})
+console.log("connect wss");
 
-console.log("listen server 9000")
+ShareDB.types.register(require("rich-text").type);
+var backend = new ShareDB();
+webSocketServer.on("connection", (webSocket) => {
+  var stream = new WebSocketJSONStream(webSocket);
+  backend.listen(stream);
+});
 
-server.listen(9000)
+console.log("listen server 9000");
+
+server.listen(9000);
